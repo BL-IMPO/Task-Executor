7ddbb561fc42defb5dcde3cc0b17318800fdc8c7
BL-IMPO <nikitka,durne@gmail.com>
2023-09-14 17:28:54 +0600

    Frame with dont works Comboboxes

--- executor.py
+++ executor.py
@@ -5,8 +5,17 @@ import Lab_Works.lab_1_task_1 as l1t1
 
 class Executor:
 
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self, file_name: str, lab_works: list[str], tasks: list[int]):
+
+        self._file_name = str(file_name)
+        self.lab_works = self._init_dict(lab_works, tasks)
+
+    def _init_dict(self, lab_works: list[str], tasks: list[int]):
+        lab = dict()
+        for i in range(len(lab_works)):
+            lab[lab_works[i]] = [str(i) for i in range(1, tasks[i] + 1)]
+
+        return lab
 
     def execute(self):
         if self.file_name == 'lab_1_task_1':
@@ -18,8 +27,28 @@ class Executor:
         with open(self.file_name + '.py', 'r') as text:
             pyperclip.copy(''.join(text.readlines()))
 
+    def get_lab_works(self):
+        return list(self.lab_works.keys())
+
+    def get_tasks(self, lab_work: str):
+        try:
+            return list(self.lab_works[lab_work])
+        except:
+            return [' ']
+    @property
+    def file_name(self):
+        return self._file_name
+
+    @file_name.setter
+    def set_file(self, name):
+        self._file_name = name
 
 if __name__ == '__main__':
-    n = Executor('lab_1_task_1')
-    n.copying()
+    n = Executor('lab_1_task_1', ['lab_1', 'lab_2', 'lab_3'], [15, 10, 15])
+    print(n.get_lab_works())
+    print(n.get_tasks('lab_1'))
+    #n.copying()
     n.execute()
+    print(n.file_name)
+    n.set_file = 'lab_1_task_2'
+    print(n.file_name)
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import tkinter as tk
-
+from tkinter import ttk
+import executor
 
 class App(tk.Tk):
+
     def __init__(self):
         super().__init__()
 
-        
+        # init Executor
+        self.log = executor.Executor('lab_1_task_1', ['lab_1', 'lab_2', 'lab_3'], [15, 10, 15])
+
+        # Window Settings
+        self.geometry("600x400")
+        self.resizable(False, False)
+
+        # Choice lab work and task
+        self.lab_work_cmb = ttk.Combobox(self, values=self.log.get_lab_works(), postcommand= self.reset_task)
+        self.lab_work_cmb.grid(row=0, column=0, sticky="wesn")
+
+        print(self.lab_work_cmb.get())
+
+        self.task_cmb = ttk.Combobox(self, values=self.log.get_tasks(self.lab_work_cmb.get()))
+        self.task_cmb.grid(row=0, column=1, sticky="wesn")
+
+
+
+    def reset_task(self) -> None:
+        self.task_cmb = ttk.Combobox(self, values=self.log.get_tasks(self.lab_work_cmb.get()))
+        self.task_cmb.grid(row=0, column=1, sticky="wesn")
+        print(self.lab_work_cmb.get())
+
 if __name__ == '__main__':
     root = App()
     root.mainloop()
